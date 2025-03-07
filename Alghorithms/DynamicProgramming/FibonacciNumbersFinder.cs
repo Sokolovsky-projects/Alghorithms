7aa7a7be1044d6894756efd177215338fcd92fce
va.sokolov <sokolov_vadim95@bk.ru>
2025-03-07 22:19:30 +0300

    добавлен алгоритм динамического программирования, на примере вычисления ряда фибоначчи

---
+++ Alghorithms/DynamicProgramming/FibonacciNumbersFinder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alghorithms.DynamicProgramming
+{
+    public class FibonacciNumbersFinder
+    {
+        // основная суть динамического программирования - сохранение результатов для оптимизации вычислений
+        public readonly Dictionary<int, long> memory = new Dictionary<int, long>();
+
+        public FibonacciNumbersFinder() 
+        {
+            // запись в память первых значений
+            memory[0] = 0;
+            memory[1] = 1;
+        }
+
+        public long GetFibonacci(int n)
+        {
+            // если в памяти уже есть решение для числа то возвращаем его
+            if (memory.ContainsKey(n))
+            {
+                return memory[n];
+            }
+
+            // находим число ряда Фибоначчи, суммма двух предыдущих чисел
+            memory[n] = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+
+            return memory[n];
+        }
+
+        public void ConsoleLogMemory()
+        {
+            foreach (var item in memory) 
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+
+        public Dictionary<int, long> GetMemory()
+        {
+            return memory;
+        }
+    }
+}
---
+++ TestAlghorithms/UnitTestFibonacciFinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alghorithms.DynamicProgramming;
+
+namespace TestAlghorithms
+{
+    public class UnitTestFibonacciFinder
+    {
+        [Fact]
+        public void Fibonacci_Finder_Problem()
+        {
+            FibonacciNumbersFinder finder = new FibonacciNumbersFinder();
+            
+            Assert.Equal(1134903170, finder.GetFibonacci(45));
+            Assert.Equal(21, finder.GetFibonacci(8));
+
+            finder.ConsoleLogMemory();
+        }
+    }
+}
