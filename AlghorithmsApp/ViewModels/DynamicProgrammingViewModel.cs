a98b449e153134d23c76f5c9d57bde976cbdeac6
va.sokolov <sokolov_vadim95@bk.ru>
2025-03-31 05:38:34 +0300

    добалено динамическое программирование

---
+++ AlghorithmsApp/ViewModels/DynamicProgrammingViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alghorithms.DynamicProgramming;
+using Microsoft.Xaml.Behaviors.Core;
+
+namespace AlghorithmsApp.ViewModels
+{
+    public class DynamicProgrammingViewModel : BaseViewModel
+    {
+        public DynamicProgrammingViewModel() 
+        {
+
+        }
+
+        private int fibonacciNumber;
+        public int FibonacciNumber
+        {
+            get
+            {
+                return fibonacciNumber;
+            }
+            set
+            {
+                fibonacciNumber = value;
+                OnPropertyChanged(nameof(FibonacciNumber));
+            }
+        }
+
+        private long fibonacciAnswer;
+        public long FibonacciAnswer
+        {
+            get
+            {
+                return fibonacciAnswer;
+            }
+            set
+            {
+                fibonacciAnswer = value;
+                OnPropertyChanged(nameof(FibonacciAnswer));
+            }
+        }
+
+        private ActionCommand solveFibonacci;
+        public ActionCommand SolveFibonacci
+        {
+            get
+            {
+                if (solveFibonacci == null)
+                {
+                    solveFibonacci = new ActionCommand(Solve);
+                }
+
+                return solveFibonacci;
+            }
+        }
+
+        private void Solve()
+        {
+            FibonacciNumbersFinder fibonacciNumbersFinder = new FibonacciNumbersFinder();
+
+            FibonacciAnswer = fibonacciNumbersFinder.GetFibonacci(FibonacciNumber);
+        }
+    }
+}
