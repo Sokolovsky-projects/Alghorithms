4f4183898ea48317eb81a7d63b88da3041d37634
va.sokolov <sokolov_vadim95@bk.ru>
2025-05-19 05:49:31 +0300

    TransportTaskSolver patch 7.38с

--- Alghorithms/TransportTask/TransportTaskSolver.cs
+++ Alghorithms/TransportTask/TransportTaskSolver.cs
@@ -1,144 +1,246 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Alghorithms.TransportTask
 {
     public class TransportTaskSolver
     {
-        private double[,] costMatrix; // Матрица стоимостей
-        private double[] supply; // Массив поставок
-        private double[] demand; // Массив спроса
-
-        private double[,] resultMatrix;
-        public double[,] ResultMatrix
-        {
-            get
-            {
-                return resultMatrix;
-            }
-        }
+        private double[,] costMatrix;
+        private double[] supply;
+        private double[] demand;
+        public double[,] ResultMatrix { get; private set; }
 
         public TransportTaskSolver(double[,] costMatrix, double[] supply, double[] demand)
         {
-            this.costMatrix = costMatrix;
-            this.supply = supply;
-            this.demand = demand;
+            this.costMatrix = (double[,])costMatrix.Clone();
+            this.supply = (double[])supply.Clone();
+            this.demand = (double[])demand.Clone();
 
-            MinimumCostMethod();
-            NorthwestCornerMethod();
+            var initial = MinimumCostMethod();
+            ResultMatrix = (double[,])initial.Clone();
+            Optimize(ResultMatrix);
         }
 
-        // Метод минимальной стоимости
         public double[,] MinimumCostMethod()
         {
-            int rows = supply.Length;
-            int cols = demand.Length;
-            double[,] result = new double[rows, cols];
+            int m = supply.Length, n = demand.Length;
+            var plan = new double[m, n];
+            var costCopy = (double[,])costMatrix.Clone();
+            var sCopy = (double[])supply.Clone();
+            var dCopy = (double[])demand.Clone();
 
-            // Создаём копию матрицы стоимостей
-            double[,] costCopy = (double[,])costMatrix.Clone();
-
-            int i = 0, j = 0;
-
-            while (i < rows && j < cols)
+            while (true)
             {
                 double minCost = double.MaxValue;
-                int minRow = -1, minCol = -1;
-
-                // Ищем минимальную стоимость в матрице
-                for (int r = 0; r < rows; r++)
-                {
-                    for (int c = 0; c < cols; c++)
-                    {
-                        if (costCopy[r, c] < minCost && supply[r] > 0 && demand[c] > 0)
+                int bi = -1, bj = -1;
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                        if (costCopy[i, j] < minCost && sCopy[i] > 0 && dCopy[j] > 0)
                         {
-                            minCost = costCopy[r, c];
-                            minRow = r;
-                            minCol = c;
+                            minCost = costCopy[i, j];
+                            bi = i; bj = j;
+                        }
+                if (bi < 0) break;
+
+                double q = Math.Min(sCopy[bi], dCopy[bj]);
+                plan[bi, bj] = q;
+                sCopy[bi] -= q;
+                dCopy[bj] -= q;
+
+                if (sCopy[bi] == 0)
+                    for (int j = 0; j < n; j++)
+                        costCopy[bi, j] = double.MaxValue;
+                if (dCopy[bj] == 0)
+                    for (int i = 0; i < m; i++)
+                        costCopy[i, bj] = double.MaxValue;
+            }
+
+            return plan;
+        }
+
+        private void Optimize(double[,] sol)
+        {
+            int m = sol.GetLength(0), n = sol.GetLength(1);
+            var isAlloc = new bool[m, n];
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    if (sol[i, j] > 0)
+                        isAlloc[i, j] = true;
+
+            FixDegeneracy(isAlloc);
+
+            while (true)
+            {
+                var u = new double[m]; var v = new double[n];
+                var uSet = new bool[m]; var vSet = new bool[n];
+                uSet[0] = true;
+                bool updated;
+                do
+                {
+                    updated = false;
+                    for (int i = 0; i < m; i++)
+                        for (int j = 0; j < n; j++)
+                            if (isAlloc[i, j])
+                            {
+                                if (uSet[i] && !vSet[j])
+                                {
+                                    v[j] = costMatrix[i, j] - u[i];
+                                    vSet[j] = true;
+                                    updated = true;
+                                }
+                                if (!uSet[i] && vSet[j])
+                                {
+                                    u[i] = costMatrix[i, j] - v[j];
+                                    uSet[i] = true;
+                                    updated = true;
+                                }
+                            }
+                } while (updated);
+
+                double minDelta = 0; int enterI = -1, enterJ = -1;
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                        if (!isAlloc[i, j])
+                        {
+                            double delta = costMatrix[i, j] - u[i] - v[j];
+                            if (delta < minDelta)
+                            {
+                                minDelta = delta;
+                                enterI = i; enterJ = j;
+                            }
+                        }
+
+                if (enterI < 0) break;
+
+                var cycle = BuildCycle(enterI, enterJ, isAlloc);
+                if (cycle == null) break;
+
+                double theta = double.MaxValue;
+                for (int k = 1; k < cycle.Count; k += 2)
+                {
+                    var (ci, cj) = cycle[k];
+                    theta = Math.Min(theta, sol[ci, cj]);
+                }
+
+                for (int k = 0; k < cycle.Count; k++)
+                {
+                    var (ci, cj) = cycle[k];
+                    sol[ci, cj] += (k % 2 == 0 ? theta : -theta);
+                    if (sol[ci, cj] <= 0 && isAlloc[ci, cj] && k % 2 == 1)
+                        isAlloc[ci, cj] = false;
+                }
+                isAlloc[enterI, enterJ] = true;
+            }
+        }
+
+        private void FixDegeneracy(bool[,] isAlloc)
+        {
+            int m = isAlloc.GetLength(0), n = isAlloc.GetLength(1);
+            int need = m + n - 1;
+            int have = isAlloc.Cast<bool>().Count(x => x);
+
+            for (int i = 0; i < m && have < need; i++)
+            {
+                for (int j = 0; j < n && have < need; j++)
+                {
+                    if (!isAlloc[i, j])
+                    {
+                        int rowCount = Enumerable.Range(0, n).Count(c => isAlloc[i, c]);
+                        int colCount = Enumerable.Range(0, m).Count(r => isAlloc[r, j]);
+                        if (rowCount < 2 && colCount < 2)
+                        {
+                            isAlloc[i, j] = true;
+                            have++;
+                        }
+                    }
+                }
+            }
+        }
+
+        private List<(int, int)> BuildCycle(int si, int sj, bool[,] isAlloc)
+        {
+            int m = isAlloc.GetLength(0), n = isAlloc.GetLength(1);
+            var path = new List<(int, int)>();
+            var used = new bool[m, n];
+
+            bool Dfs(int i, int j, bool horizontal)
+            {
+                path.Add((i, j));
+                used[i, j] = true;
+                if (path.Count >= 4 && i == si && j == sj && path.Count % 2 == 0)
+                    return true;
+
+                if (horizontal)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        if ((col != j || path.Count == 1)
+                            && (isAlloc[i, col] || (i == si && col == sj))
+                            && !used[i, col])
+                        {
+                            if (Dfs(i, col, !horizontal))
+                                return true;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int row = 0; row < m; row++)
+                    {
+                        if ((row != i || path.Count == 1)
+                            && (isAlloc[row, j] || (row == si && j == sj))
+                            && !used[row, j])
+                        {
+                            if (Dfs(row, j, !horizontal))
+                                return true;
                         }
                     }
                 }
 
-                if (minRow == -1 || minCol == -1) break;
-
-                double allocation = Math.Min(supply[minRow], demand[minCol]);
-                result[minRow, minCol] = allocation;
-                supply[minRow] -= allocation;
-                demand[minCol] -= allocation;
-
-                if (supply[minRow] == 0) costCopy[minRow, minCol] = double.MaxValue; // Закрываем строку
-                if (demand[minCol] == 0) costCopy[minRow, minCol] = double.MaxValue; // Закрываем колонку
+                path.RemoveAt(path.Count - 1);
+                return false;
             }
 
-            return result;
+            // попытка сначала по горизонтали
+            if (Dfs(si, sj, true))
+                return path;
+
+            // очистка состояния для повторного поиска по вертикали
+            path.Clear();
+            for (int x = 0; x < m; x++)
+                for (int y = 0; y < n; y++)
+                    used[x, y] = false;
+
+            if (Dfs(si, sj, false))
+                return path;
+
+            return null;
         }
 
-        public double[,] NorthwestCornerMethod()
+        public double CalculateTotalCost(double[,] sol)
         {
-            int rows = supply.Length;
-            int cols = demand.Length;
-            double[,] result = new double[rows, cols];
-
-            double[] supplyCopy = (double[])supply.Clone();
-            double[] demandCopy = (double[])demand.Clone();
-
-            int i = 0, j = 0;
-
-            while (i < rows && j < cols)
-            {
-                double allocation = Math.Min(supplyCopy[i], demandCopy[j]);
-                result[i, j] = allocation;
-                supplyCopy[i] -= allocation;
-                demandCopy[j] -= allocation;
-
-                if (supplyCopy[i] == 0 && i < rows - 1) i++;
-                else if (demandCopy[j] == 0 && j < cols - 1) j++;
-                else if (supplyCopy[i] == 0 && demandCopy[j] == 0)
-                {
-                    if (i < rows - 1) i++;
-                    if (j < cols - 1) j++;
-                }
-            }
-
-            return result;
+            double total = 0;
+            for (int i = 0; i < sol.GetLength(0); i++)
+                for (int j = 0; j < sol.GetLength(1); j++)
+                    total += sol[i, j] * costMatrix[i, j];
+            return total;
         }
 
-        // Подсчёт стоимости для текущего распределения
-        public double CalculateTotalCost(double[,] solution)
+        public void PrintSolution(double[,] sol)
         {
-            double totalCost = 0;
-            int rows = supply.Length;
-            int cols = demand.Length;
-
-            for (int i = 0; i < rows; i++)
+            Console.WriteLine("Решение:");
+            for (int i = 0; i < sol.GetLength(0); i++)
             {
-                for (int j = 0; j < cols; j++)
-                {
-                    totalCost += solution[i, j] * costMatrix[i, j];
-                }
-            }
-
-            return totalCost;
-        }
-
-        // Вывод решения
-        public void PrintSolution(double[,] solution)
-        {
-            int rows = supply.Length;
-            int cols = demand.Length;
-
-            Console.WriteLine("Решение транспортной задачи:");
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    Console.Write($"{solution[i, j]} \t");
-                }
+                for (int j = 0; j < sol.GetLength(1); j++)
+                    Console.Write($"{sol[i, j],6}");
                 Console.WriteLine();
             }
+            Console.WriteLine($"Общая стоимость: {CalculateTotalCost(sol)}");
         }
     }
 }
